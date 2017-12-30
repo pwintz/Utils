69207d5b46af2daa4870fe6f7a92d3170f30cfff
Paul Wintz <paulandhisviolin@gmail.com>
2017-12-30 00:44:08 -0800

    Added FileOption

---
+++ src/paul/wintz/uioptiontypes/FileOption.java
@@ -0,0 +1,11 @@
+package paul.wintz.uioptiontypes;
+
+import java.io.File;
+
+public class FileOption extends UserInputOption<File>{
+
+	public FileOption(final String description) {
+		super(description);
+	}
+
+}
